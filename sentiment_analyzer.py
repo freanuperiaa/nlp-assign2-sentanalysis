132635308f5694eba0341627f7ef16e008bf1986
Freanu Peria <magnussonfrancis@gmail.com>
2021-07-15 11:25:07 +0800

    can now get tweets and put them in a dataframe.

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -3,6 +3,8 @@ from tweepy import Cursor
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import numpy as np
+import pandas as pd
 
 import credentials
 
@@ -14,6 +16,9 @@ class TwitterClient():
         self.twitter_client = API(self.auth)
         self.twitter_user = twitter_user
 
+    def get_twitter_client_api(self):
+        return self.twitter_client
+
     def get_user_timeline_tweets(self, num_tweets):
         tweets = []
         for tweet in Cursor(
@@ -21,6 +26,19 @@ class TwitterClient():
         ).items(num_tweets):
             tweets.append(tweet)
         return tweets
+
+    def get_friend_list(self, num_friends):
+        friend_list = []
+        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
+            friend_list.append(friend)
+        return friend_list
+
+    def get_home_timeline_tweets(self, num_tweets):
+        home_timeline_tweets = []
+        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
+            home_timeline_tweets.append(tweet)
+        return home_timeline_tweets
+
 
 
 class TwitterAuthenticator():
@@ -74,11 +92,34 @@ class TwitterListener(StreamListener):
         print(status)
 
 
-if __name__ == '__main__':
-    hash_tag_list = ['donald trump', 'hilary clinton', 'barack obama', 'berny sanders']
-    fetched_tweets_filename = "tweets.json"
 
-    twitter_client = TwitterClient('pycon')
-    print(twitter_client.get_user_timeline_tweets(1))
-    # twitter_streamer = TwitterStreamer()
-    # twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
+class TweetAnalyzer():
+    """
+    Class for analyzing content from tweets
+    """
+    def tweets_to_data_frame(self, tweets):
+        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+        df['id'] = np.array([tweet.id for tweet in tweets])
+        return df
+
+
+if __name__ == '__main__':
+    # hash_tag_list = ['donald trump', 'hilary clinton', 'barack obama', 'berny sanders']
+    # fetched_tweets_filename = "tweets.json"
+
+    # twitter_client = TwitterClient('pycon')
+    # print(twitter_client.get_user_timeline_tweets(1))
+    # # twitter_streamer = TwitterStreamer()
+    # # twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
+
+    twitter_client = TwitterClient()
+    tweet_analyzer = TweetAnalyzer()
+    api = twitter_client.get_twitter_client_api()
+    tweets = api.user_timeline(screen_name="pycon", count=20) # apprently, Donald trump is suspended
+    # print(tweets)
+
+    # for checking the properties/keys of the tweet object
+    # print(dir(tweets[0]))
+
+    df = tweet_analyzer.tweets_to_data_frame(tweets)
+    print(df.head(5))
